8115b24b9517d9a1b3eb35d0a3b8e5a128546dd4
shigemk2 <i.am.shige@gmail.com>
2016-11-06 18:26:29 +0900

    Fix typo

--- src/main/scala/com/example/MessageBus.scala
+++ src/main/scala/com/example/MessageBus.scala
@@ -7,10 +7,10 @@ object MessageBusDriver extends CompletableApp(9) {
 }
 
 case class CommandHandler(applicationId: String, handler: ActorRef)
-case class ExecuteBuyOrder(portfolioId: String, symbol: String, quantitly: Int, price: Double)
-case class BuyOrderExecuted(portfolioId: String, symbol: String, quantitly: Int, price: Double)
-case class ExecuteSellOrder(portfolioId: String, symbol: String, quantitly: Int, price: Double)
-case class SellOrderExecuted(portofolioId: String, symbol: String, quantity: Int, price: Double)
+case class ExecuteBuyOrder(portfolioId: String, symbol: String, quantity: Int, price: Double)
+case class BuyOrderExecuted(portfolioId: String, symbol: String, quantity: Int, price: Double)
+case class ExecuteSellOrder(portfolioId: String, symbol: String, quantity: Int, price: Double)
+case class SellOrderExecuted(portfolioId: String, symbol: String, quantity: Int, price: Double)
 case class NotificationInterest(applicationId: String, interested: ActorRef)
 case class RegisterCommandHandler(applicationId: String, commandId: String, handler: ActorRef)
 case class RegisterNotificationInterest(applicationId: String, notificationId: String, interested: ActorRef)
