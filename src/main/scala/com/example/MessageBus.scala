3d42b76824750838e84464ad7bd49e066114274c
shigemk2 <i.am.shige@gmail.com>
2016-11-06 18:18:08 +0900

    Fix RegisterCommandHandler

--- src/main/scala/com/example/MessageBus.scala
+++ src/main/scala/com/example/MessageBus.scala
@@ -11,7 +11,7 @@ case class ExecuteBuyOrder(portfolioId: String, symbol: String, quantitly: Int, 
 case class BuyOrderExecuted(portfolioId: String, symbol: String, quantitly: Int, price: Double)
 case class ExecuteSellOrder(portfolioId: String, symbol: String, quantitly: Int, price: Double)
 case class NotificationInterest(applicationId: String, interested: ActorRef)
-case class RegisterCommandHandler(applicationId: String, commandId: String, interested: ActorRef)
+case class RegisterCommandHandler(applicationId: String, commandId: String, handler: ActorRef)
 case class RegisterNotificationInterest(applicationId: String, notificationId: String, interested: ActorRef)
 case class TradingCommand(commandId: String, command: Any)
 case class TradingNotification(notificationId: String, notification: Any)
