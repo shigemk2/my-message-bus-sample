c8448eef04635d2791221d6a5d8b42379cd7538b
shigemk2 <i.am.shige@gmail.com>
2016-11-06 18:16:00 +0900

    Add TradingBus class

--- src/main/scala/com/example/MessageBus.scala
+++ src/main/scala/com/example/MessageBus.scala
@@ -1,5 +1,6 @@
 package com.example
 
+import akka.actor.Actor.Receive
 import akka.actor._
 
 object MessageBusDriver extends CompletableApp(9) {
@@ -16,3 +17,75 @@ case class TradingCommand(commandId: String, command: Any)
 case class TradingNotification(notificationId: String, notification: Any)
 
 case class Status()
+
+class TradingBus(canStartAfterRegistered: Int) extends Actor {
+  val commandHandlers = scala.collection.mutable.Map[String, Vector[CommandHandler]]()
+  val notificationInterests = scala.collection.mutable.Map[String, Vector[NotificationInterest]]()
+
+  var totalRegistered = 0
+
+  def dispatchCommand(command: TradingCommand) = {
+    if (commandHandlers.contains(command.commandId)) {
+      commandHandlers(command.commandId) map { commandHandler =>
+        commandHandler.handler ! command.command
+      }
+    }
+  }
+
+  def dispatchNotification(notification: TradingNotification) = {
+    if (notificationInterests.contains(notification.notificationId)) {
+      notificationInterests(notification.notificationId) map { notificationInterest =>
+        notificationInterest.interested ! notification.notification
+      }
+    }
+  }
+
+  def notifyStartWhenReady() = {
+    totalRegistered += 1
+
+    if (totalRegistered == this.canStartAfterRegistered) {
+      println(s"TradingBus: is ready with registered actors: $totalRegistered")
+      MessageBusDriver.completedStep()
+    }
+  }
+
+  override def receive: Receive = {
+    case register: RegisterCommandHandler =>
+      println(s"TradingBus: registering: $register")
+      registerCommandHandler(register.commandId, register.applicationId, register.handler)
+      notifyStartWhenReady()
+    case register: RegisterNotificationInterest =>
+      println(s"TradingBus: registering: $register")
+      registerNotificationInterest(register.notificationId, register.applicationId, register.interested)
+      notifyStartWhenReady()
+    case command: TradingCommand =>
+      println(s"TradingBus: dispatching command: $command")
+      dispatchCommand(command)
+    case notification: TradingNotification =>
+      println(s"TradingBus: dispatching notification: $notification")
+      dispatchNotification(notification)
+    case status: Status =>
+      println(s"TradingBus: STATUS: has commandHandlers: $commandHandlers")
+      println(s"TradingBus: STATUS: has notificationInterests: $notificationInterests")
+    case message: Any =>
+      println(s"TradingBus: received unexpected: $message")
+  }
+
+  def registerCommandHandler(commandId: String, applicationId: String, handler: ActorRef) = {
+    if (!commandHandlers.contains(commandId)) {
+      commandHandlers(commandId) = Vector[CommandHandler]()
+    }
+
+    commandHandlers(commandId) =
+      commandHandlers(commandId) :+ CommandHandler(applicationId, handler)
+  }
+
+  def registerNotificationInterest(notificationId: String, applicationId: String, interested: ActorRef) = {
+    if (!notificationInterests.contains(notificationId)) {
+      notificationInterests(notificationId) = Vector[NotificationInterest]()
+    }
+
+    notificationInterests(notificationId) =
+      notificationInterests(notificationId) :+ NotificationInterest(applicationId, interested)
+  }
+}
