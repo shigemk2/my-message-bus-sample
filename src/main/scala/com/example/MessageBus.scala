864c93beb05c635982e1fa2cc25bb2bb57f82df0
shigemk2 <i.am.shige@gmail.com>
2016-11-06 17:35:52 +0900

    Add case classes

--- src/main/scala/com/example/MessageBus.scala
+++ src/main/scala/com/example/MessageBus.scala
@@ -4,3 +4,15 @@ import akka.actor._
 
 object MessageBusDriver extends CompletableApp(9) {
 }
+
+case class CommandHandler(applicationId: String, handler: ActorRef)
+case class ExecuteBuyOrder(portfolioId: String, symbol: String, quantitly: Int, price: Double)
+case class BuyOrderExecuted(portfolioId: String, symbol: String, quantitly: Int, price: Double)
+case class ExecuteSellOrder(portfolioId: String, symbol: String, quantitly: Int, price: Double)
+case class NotificationInterest(applicationId: String, interested: ActorRef)
+case class RegisterCommandHandler(applicationId: String, commandId: String, interested: ActorRef)
+case class RegisterNotificationInterest(applicationId: String, notificationId: String, interested: ActorRef)
+case class TradingCommand(commandId: String, command: Any)
+case class TradingNotification(notificationId: String, notification: Any)
+
+case class Status()
